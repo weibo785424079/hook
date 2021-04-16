d456523d6f09b6d74fde06eb47c9c38dcfb34c16
bo.wei <bo.wei@mobilemd.cn>
2021-04-16 14:51:41 +0800

    chore

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? '/site-hook/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/hook/' : '/',
     title: '@tms/site-hook',
     description: 'site的hook組件库',
     outputPath: "docs_dist",
